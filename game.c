89baa02333b663b902fe244ec948fa06d9ca2117
Pedro Tavares <ph_tavares@hotmail.com>
2022-04-01 19:09:39 -0300

    Structuring the project

--- game.c
+++ game.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int dificuldade;
+	int valor1;
+	int valor2;
+	int operacao;
+	int resultado;
+}Calcular;
+
+void jogar();
+void mostrarInfo(Calcular calcular);
+int pontos = 0;
 
 int main() {
-
+	// Deve ser chamado apenas uma vez.
+	srand(time(NULL));
+	jogar();
 	return 0;
+}
+
+void jogar() {
+	//TODO Implementar
+}
+
+void mostrarInfo(Calcular calcular) {
+	//TODO Implementar
+}
+
+int somar(int resposta, Calcular calcular) {
+	//TODO Implementar
+}
+
+int diminuir(int resposta, Calcular calcular) {
+	//TODO Implementar
+}
+
+int multiplicar(int resposta, Calcular calcular) {
+	//TODO Implementar
 }
